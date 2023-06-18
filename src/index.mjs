902d6b4cd73baec9b262098b89ceb586eb056978
WilterD <wilterd18@gmail.com>
2023-06-17 23:28:18 -0400

    configurando vistas ejs

---
+++ src/index.mjs
@@ -0,0 +1,21 @@
+import express from 'express';
+import ejs from 'ejs';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Página de inicio' });
+});
+
+app.listen(3000, () => {
+  console.log('El servidor está corriendo en el puerto 3000');
+});
